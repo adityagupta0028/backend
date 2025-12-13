2694fca61900c88aab008704112b2342718ecb11
mdali8630 <mohammadali10298@gmail.com>
2025-12-13 12:51:38 +0530

    upload

--- v1/routes/Customer.js
+++ v1/routes/Customer.js
@@ -2,8 +2,14 @@ const Controller = require('../controller/customer');
 const router = require('express').Router();
 
 // Product Routes for Customers
-router.get("/getProducts", Controller.ProductController.getProducts);
 router.post("/getProducts", Controller.ProductController.getProducts);
+router.get("/getProductDetails", Controller.ProductController.getProductDetails);
+// Category Routes for Customers
+router.get("/getCategories", Controller.CategoryController.getCategories);
+// Subcategory Routes for Customers
+router.get("/getSubcategories", Controller.CategoryController.getSubcategories);
+// Category Detail Route for Customers
+router.get("/getCategoryDetail", Controller.CategoryController.getCategoryDetail);
 
 module.exports = router;
 
