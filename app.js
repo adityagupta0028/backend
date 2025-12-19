d9b794644951d9e25f17adebd55a908782f167e9
mdali8630 <mohammadali10298@gmail.com>
2025-12-20 01:21:17 +0530

    upload

--- app.js
+++ app.js
@@ -28,17 +28,11 @@ const apiRouter = express.Router();
 
 
 app.use(cors({
-  origin: [
-    'https://admin.merefunds.in'
-  ],
+  origin: ['https://admin.merefunds.in'],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
-
-// Handle preflight requests
-app.options('*', cors());
-
 
 
 app.use(responses());
